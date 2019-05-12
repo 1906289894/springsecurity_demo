9d8352d2ca050239eca131855e8c0d4381ead473
陈博洋 <chenby@zylliondata.com>
2019-05-12 21:56:18 +0800

    ☕ 更新 Java 设置

--- src/main/java/com/zylliondata/d4i/projectname/ProjectApplication.java
+++ src/main/java/com/zylliondata/d4i/projectname/ProjectApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProjectApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GatewayApplication.class, args);
+        SpringApplication.run(ProjectApplication.class, args);
     }
 }
